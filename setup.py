807d525a51030f825339629f77922c70894871bf
Alexsam14 <samalex460@gmail.com>
2025-10-26 22:31:08 +0000

    Another commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    requirements=[]
+    HYPHEN_E_DOT='-e .'
+
+    with open(file_path) as file_obj:
+        requirements=file_obj.read().split("\n")
+
+        if HYPHEN_E_DOT in requirements:
+            requirements.remove(HYPHEN_E_DOT)
+
+    return requirements
+
+
+
+setup(
+    name='unsupervised-anomaly-detection-in-industrial-inspection',
+    version='0.0.1',
+    author='Alex'
+    author_email='samalex460@gmail.com'
+    packages=find_packages(),
+    install_requires=get_requirements('requiements.txt')
+)
